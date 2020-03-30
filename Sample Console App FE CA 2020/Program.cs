060183d894e69435d63dc09679d384f7ec1bca08
ITSPaul <powell.paul@itsligo.ie>
2020-03-30 12:52:37 +0100

    Initial Commit

---
+++ Sample Console App FE CA 2020/Program.cs
@@ -0,0 +1,17 @@
+﻿using ActivityTracker;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample_Console_App_FE_CA_2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Activity.Track("Sample Project Test");
+        }
+    }
+}
