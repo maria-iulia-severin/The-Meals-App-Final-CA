c9d0fdd1be8ce56fdf4503bd134f4a4d44a6ba28
Maria Severin <S00201400@mail.itsligo.ie>
2020-04-22 21:42:25 +0300

    Adding references & minor changes

--- TheMealsApp.Classes/Food.cs
+++ TheMealsApp.Classes/Food.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TheMealsApp.Classes
+﻿namespace TheMealsApp.Classes
 {
     public class Food
     {
