8f9473bb554f868e15c15905e3c8368620e6d7ca
Maria Severin <S00201400@mail.itsligo.ie>
2020-04-23 22:24:11 +0300

    Changed case of JSON - CamelCaseProperty

--- TheMealsApp/App_Start/WebApiConfig.cs
+++ TheMealsApp/App_Start/WebApiConfig.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json.Serialization;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
@@ -12,6 +13,9 @@ namespace TheMealsApp
         {
             // Web API configuration and services
             AutofacConfig.Register();
+            //Change case of JSON - for the purpose of learning
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver =
+                new CamelCasePropertyNamesContractResolver();
             // Web API routes
             config.MapHttpAttributeRoutes();
 
