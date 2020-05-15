ab282631f117c3adc9cc3a75017778f2d7802547
Maria Severin <S00201400@mail.itsligo.ie>
2020-05-15 09:42:35 +0300

    Admin Role Added

--- ClientSide/Controllers/AccountController.cs
+++ ClientSide/Controllers/AccountController.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using ClientSide.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace ClientSide.Controllers
 {
@@ -155,6 +156,14 @@ namespace ClientSide.Controllers
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
+
+                    ////temp code
+                    //var roleStore = new RoleStore<IdentityRole>(new ApplicationDbContext());
+                    //var roleManager = new RoleManager<IdentityRole>(roleStore);
+                    //await roleManager.CreateAsync(new IdentityRole("StaffMember"));
+
+                    //await UserManager.AddToRoleAsync(user.Id, "StaffMember");
+
                     await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
                     
                     // For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=320771
